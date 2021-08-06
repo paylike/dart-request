a1ae896b6eb43055e95e0454ca843398e80d2303
Imre 'Rover' Racz <racz.imre.levente@gmail.com>
2021-08-06 16:52:08 +0200

    test(io.Http): now has proper call stack test

--- test/paylike_dart_request_test.dart
+++ test/paylike_dart_request_test.dart
@@ -42,43 +42,62 @@ void main() {
       var response = await requester.request(TEST_URL, opts);
       expect(response.statusCode, 200);
     });
-  });
-/*
+
     test('Should be able to attach queries', () async {
       var client = MockHttpClient();
+      var mockRequest = MockHttpClientRequest();
+      var mockHeaders = MockHttpHeaders();
+      var mockResponse = MockHttpClientResponse();
+
       var uri = Uri.parse(TEST_URL);
       uri.replace(queryParameters: {
         'foo': 'bar',
       });
-      when(client.get(uri, headers: {
-        'X-Client': 'dart-1',
-        'Accept-Version': '1',
-      })).thenAnswer((_) => Future.value(http.Response('Foo', 200)));
-      var opts = RequestOptions().setClient(client).setVersion(1).setQuery({
-        'foo': 'bar',
+
+      when(client.getUrl(uri)).thenAnswer((_) {
+        return Future.value(mockRequest);
       });
+      when(mockRequest.headers).thenReturn(mockHeaders);
+      when(mockHeaders.add('X-Client', 'dart-1')).thenReturn(true);
+      when(mockHeaders.add('Accept-Version', '1')).thenReturn(true);
+      when(mockRequest.close()).thenAnswer((_) {
+        return Future.value(mockResponse);
+      });
+      when(mockResponse.statusCode).thenReturn(200);
+      var opts = RequestOptions().setClient(client).setVersion(1);
       var response = await requester.request(TEST_URL, opts);
-      expect(response.body, 'Foo');
       expect(response.statusCode, 200);
     });
-
     test('Should be able to send data', () async {
       var client = MockHttpClient();
-      when(client.post(Uri.parse(TEST_URL),
-              headers: {
-                'X-Client': 'dart-1',
-                'Accept-Version': '1',
-                'Content-Type': 'application/json',
-              },
-              body: {'foo': 'bar'},
-              encoding: null))
-          .thenAnswer((_) => Future.value(http.Response('Foo', 200)));
+      var mockRequest = MockHttpClientRequest();
+      var mockHeaders = MockHttpHeaders();
+      var mockResponse = MockHttpClientResponse();
+
+      var uri = Uri.parse(TEST_URL);
+      uri.replace(queryParameters: {
+        'foo': 'bar',
+      });
+
+      when(client.postUrl(uri)).thenAnswer((_) {
+        return Future.value(mockRequest);
+      });
+      when(mockRequest.write({'foo': 'bar'})).thenReturn(true);
+      when(mockRequest.headers).thenReturn(mockHeaders);
+      when(mockHeaders.add('X-Client', 'dart-1')).thenReturn(true);
+      when(mockHeaders.add('Accept-Version', '1')).thenReturn(true);
+      when(mockHeaders.add('Content-Type', 'application/json'))
+          .thenReturn(true);
+      when(mockRequest.close()).thenAnswer((_) {
+        return Future.value(mockResponse);
+      });
+      when(mockResponse.statusCode).thenReturn(200);
       var opts = RequestOptions().setClient(client).setVersion(1).setData({
         'foo': 'bar',
       });
       var response = await requester.request(TEST_URL, opts);
-      expect(response.body, 'Foo');
+      expect(response.statusCode, 200);
       expect(response.statusCode, 200);
     });
-  }); */
+  });
 }
