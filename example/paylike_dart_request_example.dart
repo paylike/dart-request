3f65983c216fa2990ee214163e89ec06040c2a87
Imre 'Rover' Racz <racz.imre.levente@gmail.com>
2021-08-16 13:57:54 +0200

    feat(example): added more example usage

--- example/paylike_dart_request_example.dart
+++ example/paylike_dart_request_example.dart
@@ -1,5 +1,14 @@
 import 'package:paylike_dart_request/paylike_dart_request.dart';
 
+Future<void> fetchBodyAsStream(
+    PaylikeRequester requester, RequestOptions opts) async {
+  var response = await requester.request('http://foo', opts);
+  var reader = await response.getBodyReader();
+  await reader.forEach((element) {
+    print(element);
+  });
+}
+
 void main() {
   var requester = PaylikeRequester().setLog((dynamic o) => print(o));
   var opts = RequestOptions.fromClientId('dart-1')
@@ -10,9 +19,15 @@ void main() {
       .setData({
         'foo': 'bar',
       });
-  requester.request('http://foo', opts).then((value) {
-    print(value);
+  requester.request('http://foo', opts).then((response) {
+    return response.getBody();
+  }).then((body) {
+    print(body);
   }).catchError((error) {
+    print(error);
+  });
+
+  fetchBodyAsStream(requester, opts).catchError((error) {
     print(error);
   });
 }
